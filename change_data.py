ad8aeda3caf354383159592b57a6a21333f16187
«Julia» <juliya454@yandex.ru>
2024-03-28 14:55:42 +0300

    Создали и протестили функция delete_row

---
+++ change_data.py
@@ -0,0 +1,5 @@
+from return_data_file import data_file
+
+
+def change_row():
+    data, nf = data_file()
--- delete_data.py
+++ delete_data.py
@@ -3,3 +3,10 @@ from return_data_file import data_file
 
 def delete_row():
     data, nf = data_file()
+    count = len(data)
+    num_row= int(input("Введите номер строки: "))
+    del data[num_row-1]
+    with open(f'db/data_{nf}.txt', 'w', encoding='utf-8') as file:
+        file.writelines(data)
+
+#delete_row()
